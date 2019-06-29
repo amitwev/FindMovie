71fac5f484cdb6efbababa7b45d5bfee573a795c
Amit Salim <Amit.Salim@globale.local>
2019-06-29 08:40:54 +0300

    with API

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ class App extends Component {
 	constructor(){
 		super()
 		this.state ={
-			movie:[]
+			movie:[],
+			searchField: ''
 		}
 	}
     onSearchChanges = (event) => {
 		if(event.key === 'Enter'){
-			console.log(event.target.value);
+			this.setState({searchField:event.target.value});
 		}
 	}
-
+	componentDidUpdate() {
+		fetch(`https://api.themoviedb.org/3/search/tv?api_key=139d66712ddc61478b65565b75a48660&language=en-US&query=${this.state.searchField}&page=1`)
+		.then(res => res.json()) 
+		.then(result => this.setState({movie: result.results}));	
+	}
     render(){
 		return ( 
 		  <div className='tc'> 
 		    <h1 className='f1 title'>Find a movie app</h1> 
 		    <SearchBox searchChange={this.onSearchChanges}/>
-		    <Movie/>
+		    <Movie movies={this.state.movie}/>
 		  </div>
 		);
 	}
